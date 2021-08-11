92839499e398e5d9dc09feb11c5b95747e9d962e
Peter Esser <me@peter-esser.de>
2021-08-11 11:54:09 +0200

    Init utils extension

---
+++ UtilsPlugin/Reactors/DebugLogPredefinedMessageReactor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using YAB.Core.EventReactor;
+using YAB.Core.Events;
+
+namespace UtilsPlugin.Reactors
+{
+    public class DebugLogPredefinedMessageReactor : IEventReactor<DebugLogPredefinedMessageReactorConfiguration, EventBase>
+    {
+        private readonly ILogger _logger;
+
+        public DebugLogPredefinedMessageReactor(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task RunAsync(DebugLogPredefinedMessageReactorConfiguration config, EventBase evt, CancellationToken cancellationToken)
+        {
+            this._logger.LogDebug(config.DebugMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UtilsPlugin/Reactors/DebugLogPredefinedMessageReactorConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using YAB.Core.EventReactor;
+using YAB.Core.Events;
+
+namespace UtilsPlugin.Reactors
+{
+    public class DebugLogPredefinedMessageReactorConfiguration : IEventReactorConfiguration<DebugLogPredefinedMessageReactor, EventBase>
+    {
+        /// <summary>
+        /// This determines what the bot should output into its logs when this reactor is executed.
+        /// </summary>
+        public string DebugMessage { get; set; }
+    }
+}
