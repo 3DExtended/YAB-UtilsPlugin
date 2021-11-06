107b1e9bfa6e2569fc62ecc1ff7cfc59721bb388
Peter Esser <me@peter-esser.de>
2021-11-06 12:34:37 +0100

    Update Framework

--- UtilsPlugin/Reactors/DebugLogPredefinedMessageReactorConfiguration.cs
+++ UtilsPlugin/Reactors/DebugLogPredefinedMessageReactorConfiguration.cs
@@ -1,13 +1,16 @@
 ﻿using YAB.Core.EventReactor;
 using YAB.Core.Events;
+using YAB.Plugins.Injectables.Options;
 
 namespace UtilsPlugin.Reactors
 {
+    [ReactorConfigurationDescription("Will send a debug message.")]
     public class DebugLogPredefinedMessageReactorConfiguration : IEventReactorConfiguration<DebugLogPredefinedMessageReactor, EventBase>
     {
         /// <summary>
         /// This determines what the bot should output into its logs when this reactor is executed.
         /// </summary>
+        [PropertyDescription(false, "The message you want to log.")]
         public string DebugMessage { get; set; }
     }
 }
