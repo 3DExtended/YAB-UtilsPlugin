15c0b26bc21bf96982a2e792598665afd2ea2d28
Peter Esser <me@peter-esser.de>
2021-11-06 12:35:03 +0100

    Add SendHttpRequestReactor
    This can be used to generate random http requests

---
+++ UtilsPlugin/Reactors/SendHttpRequestReactor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+
+using RestSharp;
+
+using YAB.Core.EventReactor;
+using YAB.Core.Events;
+
+namespace UtilsPlugin.Reactors
+{
+    public class SendHttpRequestReactor : IEventReactor<SendHttpRequestReactorConfiguration, EventBase>
+    {
+        private readonly ILogger _logger;
+
+        public SendHttpRequestReactor(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task RunAsync(SendHttpRequestReactorConfiguration config, EventBase evt, CancellationToken cancellationToken)
+        {
+            var client = new RestClient(config.Url);
+            client.Timeout = -1;
+
+            RestRequest request = null;
+
+            switch (config.RequestMethod.ToLower())
+            {
+                case "get":
+                    request = new RestRequest(Method.GET);
+                    break;
+
+                case "post":
+                    request = new RestRequest(Method.POST);
+                    break;
+
+                case "put":
+                    request = new RestRequest(Method.PUT);
+                    break;
+
+                default:
+                    _logger.LogInformation($"You specified {config.RequestMethod} as request method but only Get, Post and Put are allowed.");
+                    return;
+            }
+
+            if (!string.IsNullOrEmpty(config.SerializedHeaders))
+            {
+                var headers = JsonConvert.DeserializeObject<Dictionary<string, string>>(config.SerializedHeaders);
+                foreach (var header in headers)
+                {
+                    request.AddHeader(header.Key, header.Value);
+                }
+            }
+            IRestResponse response = await client.ExecuteAsync(request, cancellationToken).ConfigureAwait(false);
+
+            _logger.LogInformation($"Send request and got status code {response.StatusCode}");
+        }
+    }
+}
---
+++ UtilsPlugin/Reactors/SendHttpRequestReactorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using YAB.Core.EventReactor;
+using YAB.Core.Events;
+using YAB.Plugins.Injectables.Options;
+
+namespace UtilsPlugin.Reactors
+{
+    [ReactorConfigurationDescription("Will send a request to the URL specified. You can also provide additional headers using a json format.")]
+    public class SendHttpRequestReactorConfiguration : IEventReactorConfiguration<SendHttpRequestReactor, EventBase>
+    {
+        [PropertyDescription(false, "Either Post, Get or Put")]
+        public string RequestMethod { get; set; }
+
+        [PropertyDescription(false, "The headers for the request. Can be left empty. Must be a dictionary of string to string serialized in json.")]
+        public string SerializedHeaders { get; set; }
+
+        [PropertyDescription(false, "The URL where the request should go to (including \"https://\" or \"http://\")")]
+        public string Url { get; set; }
+    }
+}
